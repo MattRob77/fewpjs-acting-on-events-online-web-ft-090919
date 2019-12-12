c86124d643680d87de637ca9d13aa67dc24d29e9
Matthew Robinson <matt_robinson1@yahoo.com>
2019-12-12 23:52:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Your code here
 let dodger = document.getElementById("dodger")
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "")
+    let left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`
+    }
+}
